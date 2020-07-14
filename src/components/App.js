67425bf401a500db9429ece25ffc4edd6c47612d
morganlindskog <65289279+morganlindskog@users.noreply.github.com>
2020-07-13 19:06:15 -0500

    fixing constructor

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react'; 
 import SearchBar from './SearchBar';
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: '10px' }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+     super(props)
+   }
+    onSearchSubmit = (term) => {
+      console.log(term);
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
